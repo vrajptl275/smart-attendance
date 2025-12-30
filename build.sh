73a9a6f71c2d9467bb1933413045bed65377c1ed
vedantpatel901 <vedantpatel3259@gmail.com>
2025-12-30 23:01:10 +0530

    Optimize build script and remove duplicate dependencies

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ set -o errexit
 python3 -m pip install --upgrade pip
 
 # Install build dependencies first
-python3 -m pip install cmake numpy
+python3 -m pip install --no-cache-dir cmake numpy
 
 # Install dlib with memory constraints
 # -j1 limits compilation to 1 core to save memory
 export CMAKE_ARGS="-D_USE_CUDA=0"
 export CMAKE_BUILD_PARALLEL_LEVEL=1
 export MAKEFLAGS="-j1"
-python3 -m pip install dlib
+python3 -m pip install --no-cache-dir dlib
 
 # Install the rest of the dependencies
-python3 -m pip install -r requirements.txt
+python3 -m pip install --no-cache-dir -r requirements.txt
