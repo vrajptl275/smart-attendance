97dbff54ca8e9eafc7b586fdd50d7d8d7723aa94
vrajptl275 <sp533013@gmail.com>
2025-12-30 22:48:40 +0530

    Use python3 -m pip for compatibility

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@
 set -o errexit
 
 # Upgrade pip
-pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
 # Install build dependencies first
-pip install cmake numpy
+python3 -m pip install cmake numpy
 
 # Install dlib with memory constraints
 # -j1 limits compilation to 1 core to save memory
 export CMAKE_ARGS="-D_USE_CUDA=0"
 export CMAKE_BUILD_PARALLEL_LEVEL=1
 export MAKEFLAGS="-j1"
-pip install dlib
+python3 -m pip install dlib
 
 # Install the rest of the dependencies
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
